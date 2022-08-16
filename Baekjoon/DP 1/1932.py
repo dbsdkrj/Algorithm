6692268b5fd1abc0e19bfcdc5c1c7f5cd23f4e8f
Yuna Jung <knu07032@kangwon.ac.kr>
2022-08-16 17:15:52 +0900

    08/12

---
+++ Baekjoon/DP 1/1932.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [list(map(int, input().split())) for i in range(n)]
+
+k=2
+for i in range(1,n):
+    for j in range(k):
+        if j ==0:
+            dp[i][j] = dp[i][j]+dp[i-1][j]
+        elif j==i:
+            dp[i][j] = dp[i][j]+dp[i-1][j-1]
+        else:
+            dp[i][j] = dp[i][j]+max(dp[i-1][j-1], dp[i-1][j])
+    k+=1
+print(max(dp[n-1]))
